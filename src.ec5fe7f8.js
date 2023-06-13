4f0dc42bcc5813c22c29dca308f26de53ba68ee0
PolinaKrasovskaya <polina.vorotnik@gmail.com>
2023-06-13 03:09:17 +0100

    Updates

--- src.ec5fe7f8.js
+++ src.ec5fe7f8.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/src.e23a69cf.js.map
+//# sourceMappingURL=/koalaart/src.ec5fe7f8.js.map
